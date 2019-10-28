4b581aa28408fc34dbbba97529897eb99d1a5c64
alexjouck <alexjouck@gmail.com>
2019-10-28 15:12:59 +0100

    added error handling for substituate.c

--- substituate.c
+++ substituate.c
@@ -17,6 +17,10 @@ static void do_i_sub_it(char *str, char old, char new, int i)
 
 void substituate(char *str, char *old, char *new)
 {
+    if (my_strlen(old) != my_strlen(new)) {
+        my_putstr("error subtitution : len of old != len of new\n");
+        exit(84);
+    }
     for (int i = 0; i < my_strlen(str); i++) {
         for (int y = 0; y < my_strlen(old); y++) {
             do_i_sub_it(str, old[y], new[y], i);
