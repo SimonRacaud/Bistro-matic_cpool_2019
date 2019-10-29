7905adb21b2368c339eccb7d6a37999f79b3d285
alexjouck <alexjouck@gmail.com>
2019-10-29 11:02:32 +0100

    added the check_syntax func + the test associated

---
+++ tests/test_syntax.c
@@ -0,0 +1,78 @@
+/*
+** EPITECH PROJECT, 2019
+** test syntax
+** File description:
+** test the check_syntax func
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+void redirect_all_stdout(void)
+{
+    cr_redirect_stdout();
+    cr_redirect_stderr();
+}
+
+Test(check_syntax_error1, check_valid, .init=redirect_all_stdout)
+{
+    char *expr = "3{4|x456y";
+    char *base = "0123456789";
+    int error = check_syntax_error(expr, base);
+
+    cr_assert(error == 0);
+}
+
+Test(check_syntax_error2, check_unmatch_par_op, .init=redirect_all_stdout)
+{
+    char *expr = "3{4|x456";
+    char *base = "0123456789";
+
+    check_syntax_error(expr, base);
+    cr_assert_stderr_eq_str("syntax error", "");
+}
+
+Test(check_syntax_error3, check_unmatch_par_cl, .init=redirect_all_stdout)
+{
+    char *expr = "3{4|456y";
+    char *base = "0123456789";
+
+    check_syntax_error(expr, base);
+    cr_assert_stderr_eq_str("syntax error", "");
+}
+
+Test(check_syntax_error4, check_misplaced_op, .init=redirect_all_stdout)
+{
+    char *expr = "3{4|x456zy";
+    char *base = "0123456789";
+
+    check_syntax_error(expr, base);
+    cr_assert_stderr_eq_str("syntax error", "");
+}
+
+Test(check_syntax_error5, check_multi_op, .init=redirect_all_stdout)
+{
+    char *expr = "3{4|x4z|56y";
+    char *base = "0123456789";
+
+    check_syntax_error(expr, base);
+    cr_assert_stderr_eq_str("syntax error", "");
+}
+
+Test(check_syntax_error5, check_divide_zero, .init=redirect_all_stdout)
+{
+    char *expr = "3{4|x4z56y}0";
+    char *base = "0123456789";
+
+    check_syntax_error(expr, base);
+    cr_assert_stderr_eq_str("syntax error", "");
+}
+
+Test(check_syntax_error5, check_op_at_the_end, .init=redirect_all_stdout)
+{
+    char *expr = "3{4|x4z56yz";
+    char *base = "0123456789";
+
+    check_syntax_error(expr, base);
+    cr_assert_stderr_eq_str("syntax error", "");
+}
